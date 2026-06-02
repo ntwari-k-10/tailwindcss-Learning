39285eb87059f812c6cbdc730129a10633ca0a83
Kevin NTWARI <ntwarik10@gmail.com>
2026-06-02 15:42:31 +0200

    Portfolio before enabling dark mode

--- Lesson-Three/build/app.js
+++ Lesson-Three/build/app.js
@@ -1,10 +1,20 @@
 const hamburger=document.querySelector("#hamburger"); 
 const menu=document.querySelector("#menu");
 const newClass=document.querySelectorAll(".newClass")
+const hLinks=document.querySelectorAll(".hLink")
 
 hamburger.addEventListener("click",()=>{
     menu.classList.toggle("hidden");
     newClass.forEach(element => {
         element.classList.toggle("bg-white");
+    })
+})
+
+hLinks.forEach(link=>{
+    link.addEventListener("click",()=>{
+        menu.classList.add("hidden");
+        newClass.forEach(element => {
+            element.classList.remove("bg-white");
+        })
     })
 })
